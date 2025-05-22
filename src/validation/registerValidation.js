9615e2e9f7ef65309108d923131c09838fd0bccc
birando <birger1@live.com>
2025-05-22 21:31:01 +0200

    feat: add sanitation and validation for register route

---
+++ src/validation/registerValidation.js
@@ -0,0 +1,53 @@
+// validators/userValidators.js
+import { body } from "express-validator";
+import userService from "../services/userService.js";
+
+const registerUserValidator = [
+  body("username")
+    .notEmpty()
+    .withMessage("Username cannot be empty")
+    .trim()
+    .escape()
+    .isLength({ min: 3, max: 16 })
+    .withMessage("Username must be between 3 and 16 characters")
+    .custom(async (value) => {
+      const user = await userService.getUserByUsername(value);
+      if (user) {
+        throw new Error("Username already exists");
+      }
+    }),
+
+  body("email")
+    .notEmpty()
+    .withMessage("Email cannot be empty")
+    .trim()
+    .isEmail()
+    .withMessage("Not a valid email address")
+    .isLength({ min: 5, max: 32 })
+    .withMessage("Email must be between 5 and 32 characters")
+    .custom(async (value) => {
+      const user = await userService.getUserByEmail(value);
+      if (user) {
+        throw new Error("A user already exists with this email address");
+      }
+      return true;
+    }),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isStrongPassword()
+    .withMessage("Password must include at least 1 lowercase, 1 uppercase, 1 number, and 1 symbol"),
+
+  body("passwordConfirmation")
+    .notEmpty()
+    .withMessage("Please confirm your password")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords do not match");
+      }
+      return true;
+    }),
+];
+
+export default registerUserValidator;
